af1fbdbf406f834d81068c29250c5c2677966264
tnguye201769 <tnguye16@confederationcollege.ca>
2022-02-04 02:50:55 -0500

    Add project files.

---
+++ Models/Favorite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace G7CP.Models
+{
+    public partial class Favorite
+    {
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ Models/ProductCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace G7CP.Models
+{
+    public partial class ProductCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
