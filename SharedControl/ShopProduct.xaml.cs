4400df65dd00d3b2cb8518ef9835ff23715ea1b3
sangnln <nsang@confederationcollege.ca>
2022-02-16 19:00:44 -0500

    MyShop Page

---
+++ SharedControl/ShopProduct.xaml.cs
@@ -0,0 +1,44 @@
+﻿using G7CP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace G7CP.SharedControl
+{
+    /// <summary>
+    /// Interaction logic for ShopProduct.xaml
+    /// </summary>
+    public partial class ShopProduct : UserControl
+    {
+        public object ProductList
+        {
+            get => (object)GetValue(ProductListProperty);
+            set => SetValue(ProductListProperty, value);
+        }
+        private static List<Product> metaProducts = new List<Product> {
+            new Product { Name="Product 1", Price=100000},
+            new Product { Name="Product 2", Price=200000},
+            new Product { Name="Product 3", Price=300000},
+            new Product { Name="Product 4", Price=400000},
+            new Product { Name="Product 5", Price=400000}
+        };
+
+        public static readonly DependencyProperty ProductListProperty =
+            DependencyProperty.Register("ProductList", typeof(object), typeof(ShopProduct), new PropertyMetadata(metaProducts));
+        public ShopProduct()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ViewModels/MyShopViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using G7CP.Models;
+
+namespace G7CP.ViewModels
+{
+    class MyShopViewModel:BaseViewModel
+    {
+        private string avatar;
+        public string Avatar
+        {
+            get { return avatar; }
+            set { avatar = value; OnPropertyChanged(); }
+        }
+        private string cover;
+        public string Cover
+        {
+            get { return cover; }
+            set { cover = value; OnPropertyChanged(); }
+        }
+        private string mail;
+        public string Mail
+        {
+            get { return mail; }
+            set { mail = value; OnPropertyChanged(); }
+        }
+        private string phone;
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; OnPropertyChanged(); }
+        }
+        private string address;
+        public string Address
+        {
+            get { return address; }
+            set { address = value; OnPropertyChanged(); }
+        }
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set { description = value; OnPropertyChanged(); }
+        }
+        private List<Product> productList;
+        public List<Product> ProductList
+        {
+            get { return productList; }
+            set { productList = value; OnPropertyChanged(); }
+        }
+
+
+        
+
+        public MyShopViewModel()
+        {
+            GoninDigitalDBContext db = DataProvider.Instance.Db;
+            var products = new List<Product>();
+            var vendors = new List<Vendor>();
+            var ketqua = from product in products
+                         join vendor in vendors on product.VendorId equals vendor.Id
+                         select product;
+            productList = ketqua.ToList();
+        }
+
+    }
+}
