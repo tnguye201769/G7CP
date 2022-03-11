c4bda16a5ee18bf02bce2575e15848a649e2424b
sangnln <nsang@confederationcollege.ca>
2022-03-11 16:24:51 -0500

    fix shared control

---
+++ Converters/ProductStatustoVisibility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+namespace G7CP.Converters
+{
+    internal class ProductStatustoVisibility: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int StatusId = (int)value;
+            if (StatusId == 4) return true;
+            return false;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
