7d8158e78f7adcef76a73a1925a6f727f304887c
tnguye201769 <75353494+tnguye201769@users.noreply.github.com>
2022-04-04 12:29:12 -0400

    Fix database

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace G7CP.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -50,9 +50,9 @@ namespace G7CP.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Server=tcp:0.tcp.ap.ngrok.io,16504;Initial Catalog=G7CPDB;Persist Security Info=F" +
-            "alse;User ID=gonindigital;Password=gonindigital;MultipleActiveResultSets=False;T" +
-            "rustServerCertificate=False;Connection Timeout=30;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Server=tcp:g7cp.database.windows.net,1433;Initial Catalog=G7CPDB;Persist Security" +
+            " Info=False;User ID=g7cp;Password=CP440@CP440;MultipleActiveResultSets=False;Enc" +
+            "rypt=True;TrustServerCertificate=False;Connection Timeout=30;")]
         public string DBconnstr {
             get {
                 return ((string)(this["DBconnstr"]));
